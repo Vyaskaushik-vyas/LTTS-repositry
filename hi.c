a96d86768fc5032304d46d692ccf51586c652986
VYAS KAUSHIK <vyaskaushiktn@gmail.com>
2021-03-30 07:49:29 +0530

    hi

--- hi.c
+++ hi.c
@@ -1,3 +1,14 @@
+/**
+ * @file hi.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-03-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include<stdio.h>
 int main()
 {
